e6585fb3bb1c48b79d949315b82e162b40063616
Mika RAMPARANY <migame.strass@gmail.com>
2023-02-27 14:38:04 +0100

    correction et ajout fonction statut sujet + fonction afficher Message (mais celui-ci ne marche pas

--- Sujet.php
+++ Sujet.php
@@ -15,7 +15,7 @@ public function __construct ( $categorie,$titre, $datePubliSujet, $auteur, $verr
     {
     $this -> titre = $titre;
     $this -> datePubliSujet = new DateTime ($datePubliSujet);
-    $this -> verrouille = true;
+    $this -> verrouille = $verrouille;
     $this -> auteur = $auteur;
     $this -> categorie = $categorie;
     $this -> categorie ->addSujet($this);
@@ -47,31 +47,66 @@ public function __construct ( $categorie,$titre, $datePubliSujet, $auteur, $verr
         $this->auteur = $auteur;
     }
 
+    public function getverrouille() {
+        return $this->verrouille;
+    }
+
+    public function setverrouille(bool $verrouille) {
+        return $this->verrouille = $verrouille;
+    }
+
     public function getMessages() {
         return $this->messages;
     }
+    public function statutSujet()
 
-    public function verrouille(bool $verrouille=true) 
+    { 
+        if ($this -> verrouille == true)
+        {
+            // 
+            echo "<td style='display:flex; justify-content:center'><p style='color:#FFF; font-size:10px; background-color:#d43c6c; width: 50px; margin:5px; padding:5px'>".mb_strtoupper("Clôturé")."</p></td>";
+        
+        }
+        else {
+            echo "<td style='display:flex; justify-content:center'><p style='color:#FFF; font-size:10px; background-color:#36bf94; width: 50px; margin:5px; padding:5px'>".mb_strtoupper("Ouvert")."</p></td>";
+        }
+    }
+
+    public function verrouille(bool $verrouille) 
     
     {
         return $this -> verrouille = $verrouille;
     }
     //Fonctions 
 
-    public function addMessage( string $message ) 
+    public function addMessage( Message $message ) 
     {
         return $this-> messages[] = $message;
     }
 
-    public function infoNombremessage()
-    {
-        return count($this-> messages);
+    public function afficherMessages(Sujet $sujet) {
+        $messages = $sujet->getMessages();
+        if ($messages) {
+            foreach ($messages as $message) {
+                echo "Contenu : " . $message->getContenu() . "<br>";
+                echo "Auteur : " . $message->getAuteur()->getPseudo() . "<br>";
+                echo "Date de publication : " . $message->getdatePubliMessage()->format('d/m/Y H:i:s') . "<br><br>";
+            }
+        } else 
+        {
+            echo "Aucun message pour ce sujet.";
+        }
     }
+    public function showInfoSujet() // on va afficher le titre du sujet, la date de création, l'auteur ainsi que les messages qui y sont liés.
 
-    public function showSujet()
+    {   echo " <br> Sujet : " ;
+        echo " " 
+        .$this  -> statutSujet() 
 
-    {
-        echo $this -> titre;
+        ." ' ".$this -> titre ." ' "
+        
+        . " : ". "créé le " . $this -> datePubliSujet->format("d/m/Y") . " par " .$this-> auteur -> getPseudo()
+        ."<br> Total messages : ". count($this-> messages)."<br>";
     }
 
 public function __toString()
