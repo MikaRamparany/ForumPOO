5f49669ceef5a08b081e5123548f950dc2d34299
Mika RAMPARANY <migame.strass@gmail.com>
2023-03-01 15:36:42 +0100

    confection css et font pour afficherLIsteCat

--- GestionCat.php
+++ GestionCat.php
@@ -18,13 +18,19 @@ class GestionCat
         return $this->categories[] = $categorie;
     }
 
-    public function afficherListeCategories() // permet d'afficher la liste de toutes les catégories en utilisant une boucle foreach pour parcourir le tableau $categories.
-    { 
-         echo" Voici toutes les catégories ";
-         
+    public function afficherListeCategories()
+    {
+        echo "<link rel='stylesheet' href='menuListe.css' />";
+        echo "<div class='menu-titre'>Voici toutes nos catégories</div>"
+        ."<ul class='menu-deroulant'>";
+        
         foreach ($this->categories as $categorie) {
-            echo "<div class='menu-deroulant'>"
-             ."<li>" . $categorie->getLibelle() . "</li>" ."</div>";
+            echo "<li>" . $categorie->getLibelle() . "</li>";
         }
+        
+        echo "</ul>";
     }
+    
+
+   
 }
